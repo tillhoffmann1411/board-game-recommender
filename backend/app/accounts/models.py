50187a12a37aa0c91b7ca0dc6274a80e3354b9ce
Till Hoffmann <tillhoffmann1411@gmail.com>
2021-01-09 23:42:01 +0100

    :card_file_box: implemented fundamental database schema

--- backend/app/accounts/models.py
+++ backend/app/accounts/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 from games.models import BoardGame
 
 
-class Taste(models.Model):
-    games = models.ManyToManyField(BoardGame, related_name='taste')
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+class UserTaste(models.Model):
+    username = models.CharField(max_length=80)
+    number_of_reviews = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    game = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(UserTaste, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(10)])
+    text = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    origin = models.CharField(max_length=10)
