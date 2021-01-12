44d2076dd43ed964fc7dff62fe08285c0664136c
Till Hoffmann <tillhoffmann1411@gmail.com>
2021-01-12 17:09:00 +0100

    :card_file_box: changed many fields to nullable

--- backend/app/accounts/models.py
+++ backend/app/accounts/models.py
@@ -7,14 +7,14 @@ from games.models import BoardGame
 
 class UserTaste(models.Model):
     username = models.CharField(max_length=80)
-    number_of_reviews = models.PositiveIntegerField()
+    number_of_reviews = models.PositiveIntegerField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Review(models.Model):
     game = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
     created_by = models.ForeignKey(UserTaste, on_delete=models.CASCADE)
-    rating = models.PositiveIntegerField(validators=[MaxValueValidator(10)])
-    text = models.CharField(max_length=1000)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(10)], blank=True, null=True)
+    text = models.CharField(max_length=1000, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    origin = models.CharField(max_length=10)
+    origin = models.CharField(max_length=10, blank=True, null=True)
--- backend/app/games/models.py
+++ backend/app/games/models.py
@@ -4,49 +4,49 @@ from django.db.models.fields.related import ForeignKey
 
 
 class Author(models.Model):
-    id = models.CharField(max_length=80, primary_key=True)
-    name = models.CharField(max_length=80)
-    bga_url = models.URLField(max_length=200)
+    id = models.CharField(max_length=256, primary_key=True)
+    name = models.CharField(max_length=256)
+    bga_url = models.URLField(max_length=200, blank=True, null=True)
 
 
 class Publisher(models.Model):
-    id = models.CharField(max_length=80, primary_key=True)
-    name = models.CharField(max_length=80)
-    bga_url = models.URLField(max_length=200)
+    id = models.CharField(max_length=256, primary_key=True)
+    name = models.CharField(max_length=256)
+    bga_url = models.URLField(max_length=200, blank=True, null=True)
 
 
 class GameMechanic(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=80)
-    bga_id = models.CharField(max_length=80)
-    bga_name = models.CharField(max_length=80)
-    bga_url = models.URLField(max_length=200)
-    bgg_id = models.PositiveIntegerField()
-    bgg_name = models.CharField(max_length=80)
+    name = models.CharField(max_length=256)
+    bga_id = models.CharField(max_length=256, blank=True, null=True)
+    bga_name = models.CharField(max_length=256, blank=True, null=True)
+    bga_url = models.URLField(max_length=256, blank=True, null=True)
+    bgg_id = models.PositiveIntegerField(blank=True, null=True)
+    bgg_name = models.CharField(max_length=256, blank=True, null=True)
 
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=80)
-    bga_id = models.CharField(max_length=80)
-    bga_name = models.CharField(max_length=80)
-    bga_url = models.URLField(max_length=200)
-    bgg_id = models.PositiveIntegerField()
-    bgg_name = models.CharField(max_length=80)
+    name = models.CharField(max_length=256)
+    bga_id = models.CharField(max_length=256, blank=True, null=True)
+    bga_name = models.CharField(max_length=256, blank=True, null=True)
+    bga_url = models.URLField(max_length=256, blank=True, null=True)
+    bgg_id = models.PositiveIntegerField(blank=True, null=True)
+    bgg_name = models.CharField(max_length=256, blank=True, null=True)
 
 
 class OnlineGame(models.Model):
     # main
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=80)
-    description = models.CharField(max_length=1000)
-    image_url = models.URLField(max_length=200)
-    year_published = models.PositiveIntegerField()
-    min_playtime = models.PositiveSmallIntegerField()
-    max_playtime = models.PositiveSmallIntegerField()
-    min_number_of_players = models.PositiveSmallIntegerField()
-    max_number_of_players = models.PositiveSmallIntegerField()
-    min_age = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+    year_published = models.PositiveIntegerField(blank=True, null=True)
+    min_playtime = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_playtime = models.PositiveSmallIntegerField(blank=True, null=True)
+    min_number_of_players = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_number_of_players = models.PositiveSmallIntegerField(blank=True, null=True)
+    min_age = models.PositiveSmallIntegerField(blank=True, null=True)
 
     # Relations
     author = ForeignKey(Author, on_delete=models.SET_NULL, blank=True, null=True)
@@ -58,15 +58,15 @@ class OnlineGame(models.Model):
 class BoardGame(models.Model):
     # main
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=80)
-    description = models.CharField(max_length=1000)
-    image_url = models.URLField(max_length=200)
-    year_published = models.PositiveIntegerField()
-    min_playtime = models.PositiveSmallIntegerField()
-    max_playtime = models.PositiveSmallIntegerField()
-    min_number_of_players = models.PositiveSmallIntegerField()
-    max_number_of_players = models.PositiveSmallIntegerField()
-    min_age = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+    year_published = models.PositiveIntegerField(blank=True, null=True)
+    min_playtime = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_playtime = models.PositiveSmallIntegerField(blank=True, null=True)
+    min_number_of_players = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_number_of_players = models.PositiveSmallIntegerField(blank=True, null=True)
+    min_age = models.PositiveSmallIntegerField(blank=True, null=True)
 
     # Relations
     author = ForeignKey(Author, on_delete=models.SET_NULL, blank=True, null=True)
@@ -74,30 +74,30 @@ class BoardGame(models.Model):
     game_mechanic = ForeignKey(GameMechanic, on_delete=models.SET_NULL, blank=True, null=True)
     category = ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     # Similar Online Games
-    similar_online_games = models.ManyToManyField(OnlineGame, through='SimilarBoardOnlineGame')
+    similar_online_games = models.ManyToManyField(OnlineGame, through='SimilarBoardOnlineGame', blank=True, null=True)
 
     # BGG stuff
-    bgg_id = models.IntegerField()
-    bgg_avg_rating = models.FloatField()
-    bgg_num_ratings = models.PositiveIntegerField()
-    bgg_avg_bayes = models.FloatField()
-    bgg_rank = models.PositiveSmallIntegerField()
-    bgg_stddev = models.FloatField()
-    bgg_num_user_comments = models.PositiveSmallIntegerField()
+    bgg_id = models.IntegerField(blank=True, null=True)
+    bgg_avg_rating = models.FloatField(blank=True, null=True)
+    bgg_num_ratings = models.PositiveIntegerField(blank=True, null=True)
+    bgg_avg_bayes = models.FloatField(blank=True, null=True)
+    bgg_rank = models.PositiveSmallIntegerField(blank=True, null=True)
+    bgg_stddev = models.FloatField(blank=True, null=True)
+    bgg_num_user_comments = models.PositiveSmallIntegerField(blank=True, null=True)
 
     # BGA stuff
-    bga_id = models.CharField(max_length=80)
-    bga_avg_rating = models.FloatField()
-    bga_num_ratings = models.PositiveIntegerField()
-    bga_url = models.URLField(max_length=200)
-    bga_rank = models.IntegerField()
-    bga_rank_trending = models.PositiveSmallIntegerField()
-    bga_price_us = models.DecimalField(max_digits=5, decimal_places=2)
+    bga_id = models.CharField(max_length=256, blank=True, null=True)
+    bga_avg_rating = models.FloatField(blank=True, null=True)
+    bga_num_ratings = models.PositiveIntegerField(blank=True, null=True)
+    bga_url = models.URLField(max_length=200, blank=True, null=True)
+    bga_rank = models.IntegerField(blank=True, null=True)
+    bga_rank_trending = models.PositiveSmallIntegerField(blank=True, null=True)
+    bga_price_us = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
 
     # others
-    thumbnail_url = models.URLField(max_length=200)
-    official_url = models.URLField(max_length=200)
-    reddit_all_time_count = models.PositiveSmallIntegerField()
+    thumbnail_url = models.URLField(max_length=200, blank=True, null=True)
+    official_url = models.URLField(max_length=200, blank=True, null=True)
+    reddit_all_time_count = models.PositiveSmallIntegerField(blank=True, null=True)
 
 
 class SimilarBoardOnlineGame(models.Model):
