9681a55bbc6acb02953c64a8d2d69b70e1573f4e
maxmaiberger <66006918+maxmaiberger@users.noreply.github.com>
2021-01-15 01:11:40 +0100

    finished pipelines
    finished first preparation, modeling and evaluation pipeline

--- import/recommender/data_modeling.py
+++ import/recommender/data_modeling.py
@@ -1,11 +1,52 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 
 def global_average_prediction(data):
-    calculation_data = data.replace(0, np.NaN)
-    global_average = calculation_data.mean()
+    #  get number of nonzero values (number of ratings)
+    number_ratings = sum(data.count(axis=0))
+    # get sum of all column sums
+    sum_all_ratings = sum(data.sum(axis=0))
+    # calculate global average
+    global_average = sum_all_ratings / number_ratings
+    print('---global average: ', global_average)
 
-    print('--- global mean: ', global_average)
-    return global_average
+    # set 0 values to global average
+    data[data.isna()] = global_average
+
+    return data
+
+
+def game_average_prediction(data):
+    # count ratings per game column
+    count_ratings = data.count(axis=0)
+    # sum ratings per game column
+    sum_ratings = data.sum(axis=0)
+
+    # calculate game average
+    game_average = sum_ratings / count_ratings
+    # set average of NaN to 0
+    game_average[np.isnan(game_average)] = 0
+    print('---game average: ', game_average)
+
+    # set game average for NaN
+    data = data.fillna(game_average)
+
+    return data
+
+
+def user_average_prediction(data):
+    # count ratings per user column
+    count_ratings = data.count(axis=1)
+    # sum ratings per user column
+    sum_ratings = data.sum(axis=1)
+
+    # calculate user average
+    user_average = sum_ratings / count_ratings
+    # set average of NaN to 0
+    user_average[np.isnan(user_average)] = 0
+    print('---game average: ', user_average)
+    # set user average for NaN
+    data = data.T.fillna(user_average).T
+
+    return data
--- import/recommender/recommender_main.py
+++ import/recommender/recommender_main.py
@@ -6,42 +6,62 @@ from evaluation import *
 def preparation_pipeline():
     # get data from database
     df = get_recommendation_data(link='../Data/Joined/Results/Reviews.csv',
-                                 sample_number_rows=5000,
+                                 sample_number_rows=350_000,
                                  random_state=41)  # default value is None
 
     # prepare data
     df = prepare_data(data=df,
-                      min_number_ratings_user=0,
-                      min_number_ratings_game=0,
-                      fill=0)
+                      min_number_ratings_user=5,  # already filter in get_recommendation_data?
+                      min_number_ratings_game=5)
 
     # split data
-    split_data = make_train_test_split(data=df,
+    data_split = make_train_test_split(data=df,
                                        train_size=0.8,
                                        test_size=0.2,
                                        seed=42)
 
-    return split_data
+    return data_split
 
 
-def modeling_pipeline(df):
+def modeling_pipeline(df, global_average, game_average, user_average):
     # get new user name for recommendation
     new_user = "Artax"  # "4Corners"  # from frontend?
 
     # get data from dict
-    train_data = df['train_data']
+    data_train = df['train_data']
     delete_position = df['delete_position']
+    data = df['original_data']
 
-    bla = global_average_prediction(data=train_data)
+    # prediction methods
+    if global_average:
+        pred = global_average_prediction(data=data_train)
+    elif game_average:
+        pred = game_average_prediction(data=data_train)
+    elif user_average:
+        pred = user_average_prediction(data=data_train)
+    else:
+        print('You have to choose a model')
+
+    # create dict for return
+    info = {'train_data': data_train,
+            'original_data': data,
+            'delete_position': delete_position,
+            'prediction': pred}
+
+    return info
 
 
-def evaluation_pipeline():
-    print("is missing")
+def evaluation_pipeline(result):
+    calculate_rmse(result)
 
 
 if __name__ == "__main__":
     data = preparation_pipeline()
-    modeling_pipeline(df=data)
-    evaluation_pipeline()
+    result = modeling_pipeline(df=data,
+                               global_average=False,
+                               game_average=False,
+                               user_average=True)
+    evaluation_pipeline(result)
+
 
 
