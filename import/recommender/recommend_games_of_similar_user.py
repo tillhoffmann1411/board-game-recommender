ec1e739fb09387584a4eb7e921622aa0174a4257
maxmaiberger <66006918+maxmaiberger@users.noreply.github.com>
2021-01-25 14:29:07 +0100

    Update recommend_games_of_similar_user.py
    two more functions

--- import/recommender/recommend_games_of_similar_user.py
+++ import/recommender/recommend_games_of_similar_user.py
@@ -1,5 +1,48 @@
 # get similar games
-from data_preparation import *
+import pandas as pd
+import numpy as np
+import gc
+
+
+def get_recommendation_data(link, min_number_ratings_game, min_number_ratings_user, size_user_sample, seed):
+    """ output:     pandas data frame """
+    # get data
+    data = pd.read_csv(link, usecols=['user_key', 'game_key', 'rating'], sep=',', header=0)
+
+    # filter games by number of reviews and sample x unique user form data
+    # keep only games with more than x reviews
+    num_reviews_game = data.loc[:, 'game_key'].value_counts()
+    data = data[data.loc[:, 'game_key'].isin(num_reviews_game.index[num_reviews_game.gt(min_number_ratings_game)])]
+    # keep only user with more than x reviews
+    num_reviews_user = data.loc[:, 'user_key'].value_counts()
+    data = data[data.loc[:, 'user_key'].isin(num_reviews_user.index[num_reviews_user.gt(min_number_ratings_user)])]
+    # take only sample of users
+    user_sample = pd.Series(data['user_key'].unique())
+    user_sample = user_sample.sample(n=size_user_sample, replace=False, random_state=seed)
+    user_sample = user_sample.tolist()
+    data = data[data.user_key.isin(user_sample)]
+
+    print('---data shape: ', data.shape)
+
+    return data
+
+
+def prepare_data(data):
+    """ output:     pandas data frame - utility matrix """
+    # remove duplicates
+    data = data.drop_duplicates(subset=['user_key', 'game_key'], keep='last')
+
+    # create utility matrix
+    data_pivot = data.pivot(index='user_key', columns='game_key', values='rating')
+
+    # clean memory
+    del data
+    gc.collect()
+
+    # info
+    print('---pivot data shape: ', data_pivot.shape)
+
+    return data_pivot
 
 
 def calculate_centered_cosine_similarity(data, new_user):
